08db43d8be2f49179f65ac09e103145380d53d6d
Kamil S <hohlik741@gmail.com>
2018-01-30 23:25:57 +0100

    Correct gitignore and remove unnecessary lines

--- import.py
+++ import.py
@@ -1,31 +1,4 @@
-import urllib.request
 import re
-
-# urlLogin = "https://www.etutor.pl/account/login"
-# https://www.etutor.pl/words/user-words
-
-
-# html = urllib.request.urlopen(urlLogin).read().decode()
-# data = re.findall("""<span class="hw">(.*?)<span class""", opened, re.DOTALL)[-1]
-
-
-"""
-import zipfile, re
-
-f = zipfile.ZipFile("pliki_tekstowe/channel.zip")
-num = "90052"
-comments = []
-while True:
-    content = f.read(num + ".txt").decode("utf-8")
-    comments.append(f.getinfo(num + ".txt").comment.decode("utf-8"))
-    print(content)
-    match = re.search("Next nothing is (\d+)", content)
-    if match == None:
-        break
-    num = match.group(1)
-
-print("".join(comments))
-"""
 
 with open('diki1.txt') as f:
     read_data = f.read()
